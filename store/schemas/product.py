f9e942e01c1e69e10b0dcaa6a355cf85bb1558d4
david.evaristo <david.evaristo@altbank.co>
2024-06-30 22:00:34 -0300

    feat: add schemas tests

---
+++ store/schemas/product.py
@@ -0,0 +1,9 @@
+from store.schemas.base import BaseSchemaMixin
+from pydantic import Field
+
+
+class ProductIn(BaseSchemaMixin):
+    name: str = Field(..., description="Product name")
+    quantity: int = Field(..., description="Product quantity")
+    price: float = Field(..., description="Price product")
+    status: bool = Field(..., description="Status product")
